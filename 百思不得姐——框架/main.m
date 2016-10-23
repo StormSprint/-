b86350cafda42b947b47da66fdd3917c1505fb05
StormSprint <13330331104@189.cn>
2016-10-23 19:36:38 +0800

    Initial Commit

---
+++ 百思不得姐——框架/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐——框架
+//
+//  Created by 阮玉强 on 2016/10/23.
+//  Copyright © 2016年 ryq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ 百思不得姐——框架Tests/_________Tests.m
@@ -0,0 +1,39 @@
+//
+//  _________Tests.m
+//  百思不得姐——框架Tests
+//
+//  Created by 阮玉强 on 2016/10/23.
+//  Copyright © 2016年 ryq. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _________Tests : XCTestCase
+
+@end
+
+@implementation _________Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
